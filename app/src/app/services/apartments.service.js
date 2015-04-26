baf6328209f313059d30a9d67172585204608e41
Teemu Kokkonen <teemu@fastmonkeys.com>
2015-04-26 14:50:55 +0300

    plo

--- app/src/app/services/apartments.service.js
+++ app/src/app/services/apartments.service.js
@@ -1,17 +1,15 @@
 'use strict';
 
-console.log("ApartmentService initializing");
 class ApartmentService {
     constructor($http) {
         console.log("construct");
         this.$http = $http;
     }
     getAds(maxNumberOfStops, maxWalkingDistance) {
-        return this.$http.get('example.json', {
-            //cache: true,
+        return this.$http.get('http://aww.fi:4000', {
+            cache: true,
             params: {
-                maxNumberOfStops,
-                maxWalkingDistance
+                url: 'http://kumartest.cloudapp.net:8080/hackserver-0.1/dataserver.jsp?lat=60.187078&lng=24.815548&maxStop=' + maxNumberOfStops +'&maxWalk=' + maxWalkingDistance + '&callback=claudio'
             }
         });
     }
